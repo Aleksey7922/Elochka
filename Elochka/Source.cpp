0bd67a73ffd1a03212d65b8fdaad9fd1c23863d8
HP Pavilion <HP Pavilion@DESKTOP-9CQK3QQ>
2022-10-07 20:24:05 +0300

    Add project files.

---
+++ Elochka/Source.cpp
@@ -0,0 +1,32 @@
+#include<iostream>\n
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL,"Russian");
+	cout << "\t\t\t\t\t\"Ёлочка\"\n\n";
+	cout << "\t\t\t\tВ лесу родилась ёлочка," << endl;
+	cout << "\t\t\t\tВ лесу она росла," << endl;
+	cout << "\t\t\t\tЗимой и летом стройная," << endl;
+	cout << "\t\t\t\tЗеленая была.\n\n";
+	cout << "\t\t\t\tМетель ей пела песенку:" << endl;
+	cout << "\t\t\t\t\"Спи,ёлочка,бай-бай!\"" << endl;
+	cout << "\t\t\t\tМороз снежком укутывал:" << endl;
+	cout << "\t\t\t\t\"Смотри,не замерзай!\"\n\n";
+	cout << "\t\t\t\tТрусишка зайка серенький" << endl;
+	cout << "\t\t\t\tПод ёлочкой скакал" << endl;
+	cout << "\t\t\t\tПорою волк,сердитый волк" << endl;
+	cout << "\t\t\t\tРысцою пробегал.\n\n";
+	cout << "\t\t\t\tЧу!Снег по лесу частому" << endl;
+	cout << "\t\t\t\tПод полозом скрипит," << endl;
+	cout << "\t\t\t\tЛошадка мохноногая " << endl;
+	cout << "\t\t\t\tТоропится,бежит.\n\n";
+	cout << "\t\t\t\tВезет лошадка  дровеньки," << endl;
+	cout << "\t\t\t\tА в дровнях старичок," << endl;
+	cout << "\t\t\t\tСрубил  он нашу ёлочку" << endl;
+	cout << "\t\t\t\tПод самый корешок.\n\n";
+	cout << "\t\t\t\tТеперь она,нарядная," << endl;
+	cout << "\t\t\t\tНа праздник к нам пришла." << endl;
+	cout << "\t\t\t\tИ много,много радости" << endl;
+	cout << "\t\t\t\tДетишкам принесла." << endl;
+}
